88ecf6b34c4a232d8d69cc45387dc8e02dd121f4
Norman Gilmore <git@teamforecast.com>
2018-12-19 17:31:14 -0800

    Require TAGWORKS_REPO to be set in shell for step 5.

--- step5_upload_to_s3/sync_to_s3.sh
+++ step5_upload_to_s3/sync_to_s3.sh
@@ -2,6 +2,8 @@
 # This demonstrates how to sync a TagWorks corpus to an S3 bucket.
 # You will need to add an AWS key with appropriate S3 permissions to
 # ~/.aws/config labeled "corpus-writer"
-TAGWORKS_REPO="CUSTOMER_SUBDOMAIN"
+
+# Set this variable in your shell
+#export TAGWORKS_REPO="CUSTOMER_SUBDOMAIN"
 TAGWORKS_WRITER="corpus-writer"
 aws s3 sync --profile ${TAGWORKS_WRITER} data/ s3://tagworks.thusly.co/${TAGWORKS_REPO}/corpus/ --exclude "core-nlp.json.gz"
